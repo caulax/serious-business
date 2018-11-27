bfeb7ba9da6657c96250850832e3150185954f21
Alexey Burykin <alexey.burykin@norse.digital>
2018-11-27 23:51:24 +0200

    change attributes in models

--- src/src/AppBundle/Entity/Order.php
+++ src/src/AppBundle/Entity/Order.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Doctrine\Common\Collections\ArrayCollection;
 /**
- * @ORM\Table(name="order")
+ * @ORM\Table(name="e_order")
  * @ORM\Entity(repositoryClass="AppBundle\Entity\Repository\OrderRepository")
  */
 class Order
@@ -19,6 +19,7 @@ class Order
     private $id;
 
     /**
+     * @ORM\Column(name="id_user", type="string", length=255)
      * @ORM\OneToOne(target="User")
      * @ORM\JoinColumn(name="user", referencedColumnName="id")
      */
@@ -28,6 +29,12 @@ class Order
      * @ORM\Column(type="string", length=255)
      */
     private $status;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(name="time", type="datetime", nullable=false)
+     */
+    private $time = 'CURRENT_TIMESTAMP';
 
     /**
      * @return mixed
@@ -77,7 +84,27 @@ class Order
         $this->status = $status;
     }
 
+    /**
+     * Set time
+     *
+     * @param \DateTime $time
+     *
+     * @return LogLockKey
+     */
+    public function setTime($time)
+    {
+        $this->time = $time;
 
+        return $this;
+    }
 
-
+    /**
+     * Get time
+     *
+     * @return \DateTime
+     */
+    public function getTime()
+    {
+        return $this->time;
+    }
 }
